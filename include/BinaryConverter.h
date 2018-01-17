86aef468b907a81cffdddba2e64fa396751486ec
pseute <pseut001@odu.edu>
2018-01-17 12:56:55 -0500

    Remoted using namespace std from header file

--- include/BinaryConverter.h
+++ include/BinaryConverter.h
@@ -3,17 +3,15 @@
 
 #include <string>
 
-using namespace std;
-
 class BinaryConverter
 {
 public:
     string toBinary(int num);
-    int toDecimal(string binary);
-    bool verifyBinary(string binary);
-    bool verifyDecimal(string dec);
+    int toDecimal(std::string binary);
+    bool verifyBinary(std::string binary);
+    bool verifyDecimal(std::string dec);
     int charToInt(char c);
-    int strToInt(string s);
+    int strToInt(std::string s);
 };
 
 #endif // BINARYCONVERTER_H
